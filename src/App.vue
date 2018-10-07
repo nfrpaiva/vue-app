a97aa208497eb4c6a05679dc02752719f61fc658
Nilton Paiva <nfrpaiva@gmail.com>
2018-10-07 00:02:00 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
-    <header class="showcase">
-      <div class="content">
-        <img src="./assets/logo.svg" alt="" class="logo">
-        <div class="title">Lorem, ipsum.</div>
-        <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, praesentium?</div>
-        <a href="#container" class="btn btn-outline-primary">Começar</a>
-      </div>
-    </header>
+    <show-case/>
     <div class="container" id="container">
       <Post v-if="editando" :contato="item" v-on:add-contato="addPost"/>
       <div class="float-right">
@@ -23,6 +16,7 @@
 
 <script>
 import Contatos from "./components/Contatos.vue";
+import ShowCase from "./components/ShowCase";
 import Post from "./components/Post.vue";
 import axios from "axios";
 
@@ -30,7 +24,8 @@ export default {
   name: "app",
   components: {
     Contatos,
-    Post
+    Post,
+    ShowCase
   },
   created() {
     this.carregar();
@@ -89,70 +84,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.showcase::after {
-  content: "";
-  height: 100vh;
-  width: 100%;
-  background-image: url("./assets/background.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  display: block;
-  filter: blur(5px);
-  -webkit-filter: blur(5px);
-  transition: all 1000ms;
-}
-.showcase:hover::after {
-  filter: blur (0px);
-  -webkit-filter: blur(0px);
-}
-.showcase:hover .content {
-  /* filter: blur (2px);
-  -webkit-filter: blur(2px); */
-}
-.showcase .content a {
-  color: aliceblue;
-  border-color: aliceblue;
-  margin-top: 1rem;
-}
-.showcase:hover .content a {
-  background-color: unset;
-}
-.content {
-  position: absolute;
-  z-index: 1;
-  top: 10%;
-  left: 50%;
-  margin-top: 0px;
-  margin-left: -150px;
-  width: 300px;
-  height: 350px;
-  color: azure;
-  text-align: center;
-  transition: all 1000ms;
-}
-.content .logo {
-  height: 180px;
-  width: 180px;
-}
-#container {
-  transition: all 1000ms;
-}
-.content .title {
-  font-size: 2.2rem;
-  margin-top: 1rem;
-}
-.content .text {
-  line-height: 2rem;
-  margin-top: 1rem;
-}
-body {
-  margin: 0px;
-}
-html {
-  font-family: Arial, Helvetica, sans-serif;
-}
-</style>
-
---
+++ src/components/ShowCase.vue
@@ -0,0 +1,75 @@
+<template>
+    <header class="showcase">
+      <div class="content">
+        <img src="../assets/logo.svg" alt="" class="logo">
+        <div class="title">Lorem, ipsum.</div>
+        <div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, praesentium?</div>
+        <a href="#container" class="btn btn-outline-primary">Começar</a>
+      </div>
+    </header>
+</template>
+
+<style>
+.showcase::after {
+  content: "";
+  height: 100vh;
+  width: 100%;
+  background-image: url("../assets/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: block;
+  filter: blur(5px);
+  -webkit-filter: blur(5px);
+  transition: all 1000ms;
+}
+.showcase {
+  margin-bottom: 1rem;
+}
+.showcase:hover::after {
+  filter: blur (0px);
+  -webkit-filter: blur(0px);
+}
+.showcase .content a {
+  color: aliceblue;
+  border-color: aliceblue;
+  margin-top: 1rem;
+}
+.showcase:hover .content a {
+  background-color: unset;
+}
+.content {
+  position: absolute;
+  z-index: 1;
+  top: 10%;
+  left: 50%;
+  margin-top: 0px;
+  margin-left: -150px;
+  width: 300px;
+  height: 350px;
+  color: azure;
+  text-align: center;
+  transition: all 1000ms;
+}
+.content .logo {
+  height: 180px;
+  width: 180px;
+}
+#container {
+  transition: all 1000ms;
+}
+.content .title {
+  font-size: 2.2rem;
+  margin-top: 1rem;
+}
+.content .text {
+  line-height: 2rem;
+  margin-top: 1rem;
+}
+body {
+  margin: 0px;
+}
+html {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
